c28009093d121cae3168c83a312a3a64c3cba60e
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-11-09 11:33:19 -0700

    mod3 assessment with brittany

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Answer = (props) => {
-  const { id, text } = props;
+  const { id, text, score } = props;
 
   return(
-    <div>
+    <div onClick={() =>props.setSelectedAnswer(id, score)}>
       <label>
         <input type="radio" name={id} />
         {text}
